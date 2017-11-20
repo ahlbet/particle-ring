b48e55afee2800e14b1964d4fcdc43e201120e03
Dylan Stratton <dm.stratton94@gmail.com>
2017-11-20 13:19:52 -0600

    second commit

---
+++ sketch.js
@@ -0,0 +1,90 @@
+let particles = [];
+const num = 1000;
+const radius = 175;
+let startX; 
+let startY;
+
+function polToCart(r, angle) {
+	return createVector(r * cos(angle), r * sin(angle));
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	translate(width/2, height/2);
+	seed();
+}
+
+function draw() {
+	let t = frameCount;
+	translate(width/2, height/2);
+	for (let i = 0; i < particles.length; i++) {
+		particles[i].show();
+		particles[i].move(i);
+		particles[i].fade();
+		
+		if (t > 500) {
+			particles = [];
+		}
+	}
+}
+
+function seed() {
+	translate(width/2, height/2);
+	const spacing = 360 / num;
+	for (let a = 0; a < 360; a+=spacing) {
+		let point = polToCart(radius, a);
+		startX = point.x;
+		startY = point.y;
+		let particle = new Particle(point.x, point.y, 1, 255, a, startX, startY);
+		particles.push(particle);
+	}
+}
+
+class Particle {
+	constructor(x, y, r, op, a, startX, startY) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.op = op;
+		this.a = a;
+		this.startX = startX;
+		this.startY = startY;
+		this.slow = 600;
+		this.mov = .2;
+		this.fadeT = 1;
+		this.isFading = true;
+	}
+
+	show() {
+		noStroke();
+		fill(this.op, this.op, 255, 10);
+		ellipse(this.x, this.y, this.r);
+	}
+
+	move(i) {
+		let t = frameCount / 100.0;
+		const wonkV = 100000;
+
+		let wonk1 = map(sin(this.a * i), -1, 1, -wonkV, wonkV);
+		let wonk2 = map(cos(this.a * i), -1, 1, -wonkV, wonkV);
+
+		this.x += map(noise(t, wonk2 / this.slow), 0, 1, -this.mov, this.mov*1.1);
+		this.y += map(noise(wonk1 / this.slow, t), 0, 1, -this.mov, this.mov*1.1);
+	}
+
+	fade() {
+		if (this.isFading) {
+			this.op -= this.fadeT;
+		} else {
+			this.op += this.fadeT;
+		}
+		if (this.op > 255) {
+			this.isFading = true;
+		}
+		if (this.op < 200) {
+			this.isFading = false;
+		}
+	}
+
+}
